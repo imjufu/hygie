674b549bd09cd17c45dd4f3d10f101b8c8d3d807
Julien F. <julien@jufu-dynamics.com>
2024-04-22 14:19:27 +0200

    Re-evaluate activities

--- app/models/calorie_calculator.rb
+++ app/models/calorie_calculator.rb
@@ -9,10 +9,11 @@ class CalorieCalculator
   }.freeze
 
   ACTIVITIES = {
-    sedentary: 1.55,
-    moderately_active: 1.85,
-    active: 2.2,
-    very_active: 2.4
+    sedentary: 1.2,
+    lightly_active: 1.375,
+    moderately_active: 1.55,
+    active: 1.725,
+    very_active: 1.9,
   }.freeze
 
   attr_accessor :gender, :age, :height, :weight, :activity
@@ -22,17 +23,18 @@ class CalorieCalculator
   validates :activity, inclusion: { in: ACTIVITIES.keys.map(&:to_s) }
 
   def tdee
-    @tdee ||= calculate_tdee
+    @tdee ||= calculate_tdee.to_i
   end
 
   def bmr
-    @bmr ||= calculate_bmr
+    @bmr ||= calculate_bmr.to_i
   end
 
   private
 
   def calculate_bmr
     # BMR = Basal Metabolic Rate
+    # Based on the Mifflin-St Jeor Formula: https://pubmed.ncbi.nlm.nih.gov/2305711/
     return false unless valid?
 
     (10 * weight.to_i) + (6.25 * height.to_i) - (5 * age.to_i) + GENDERS.fetch(gender.to_sym)
